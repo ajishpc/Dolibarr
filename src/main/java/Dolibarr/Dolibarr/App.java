1f1b5acb59780b9433b7f7ddde89e1255129328f
ajishpc <ajishpc@users.noreply.github.com>
2019-04-19 22:54:37 +0200

    this is the first commit

---
+++ src/main/java/Dolibarr/Dolibarr/App.java
@@ -0,0 +1,13 @@
+package Dolibarr.Dolibarr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
